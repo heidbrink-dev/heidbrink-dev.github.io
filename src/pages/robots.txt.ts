22a7caf6275bb72d2e3892439050834ce922b3dc
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-25 13:49:23 +0700

    Updated robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,11 +1,10 @@
 import type { APIRoute } from "astro";
 
-const getRobotsTxt = (sitemapURL: URL) => `
-User-agent: *
+const getRobotsTxt = (sitemapURL: URL) =>
+  `User-agent: *
 Allow: /
 
-Sitemap: ${sitemapURL.href}
-`;
+Sitemap: ${sitemapURL.href}`;
 
 export const GET: APIRoute = ({ site }) => {
   const sitemapURL = new URL("sitemap-index.xml", site);
