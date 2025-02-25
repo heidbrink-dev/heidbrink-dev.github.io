4c5612d218d97fde0100a92f94df585925ff8d3a
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-02-25 12:07:56 +0700

    Added initial components (BlogCard, BlogGrid, BaseLayout, ...)

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    author: z.string(),
+    publishedDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string(),
+    tags: z.string().array(),
+  }),
+});
+
+export const collections = { blog };
