19eed668b98c49d60e11c7ac2ef5ff907274e000
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-06-27 11:43:22 +0700

    astro add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.heidbrink.dev",
-  integrations: [
-    sitemap()
-  ],
+  integrations: [sitemap(), mdx()],
   vite: {
     plugins: [
       tailwindcss()
