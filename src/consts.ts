286222f86fc1774c166d2bf8479a34e61fa75a5e
Niels Heidbrink <72007270+niels-hb@users.noreply.github.com>
2025-07-04 10:15:39 +0700

    Updated Footer, removed LinkedIn from SocialLinks

--- src/consts.ts
+++ src/consts.ts
@@ -5,5 +5,4 @@ export const SOCIAL_LINKS = [
   { href: "https://x.com/heidbrink_dev", icon: "/icons/x.svg", alt: "X" },
   { href: "https://github.com/heidbrink-dev", icon: "/icons/github.svg", alt: "GitHub" },
   { href: "https://www.youtube.com/@heidbrink_dev", icon: "/icons/youtube.svg", alt: "YouTube" },
-  { href: "https://www.linkedin.com/in/niels-heidbrink", icon: "/icons/linkedin.svg", alt: "LinkedIn" },
 ];
